9b07a5085d38ea08231521971f965398534dfc04
Hunter20050910 <145260700+Abrorov-javohir@users.noreply.github.com>
2024-09-13 16:54:14 +0500

    changed some function

--- bin/kalkulyator.dart
+++ bin/kalkulyator.dart
@@ -1,24 +1,18 @@
 import 'dart:io';
 
 void main() {
-  // Foydalanuvchidan ifoda olish
-  print('Ifodani kiriting (masalan: 5 + 3 - 2 * 4): ');
-  String? expression = stdin.readLineSync();
+  print('Misol kiriting: ');
+  String? ifoda = stdin.readLineSync();
 
-  // Agar kiritilgan ifoda bo'sh bo'lmasa davom etamiz
-  if (expression != null && expression.isNotEmpty) {
-    // Ifodani bo'sh joyga qarab ajratamiz
-    List<String> listlar = expression.split('');
+  if (ifoda != null && ifoda.isNotEmpty) {
+    List<String> listlar = ifoda.split('');
 
-    // Birinchi sonni o'zgartiramiz
     double sum = double.parse(listlar[0]);
 
-    // Amal va keyingi sonlarni ko'rib chiqamiz
     for (int i = 1; i < listlar.length; i += 2) {
       String operator = listlar[i];
       double nextNumber = double.parse(listlar[i + 1]);
 
-      // Amal turiga qarab hisoblaymiz
       switch (operator) {
         case '+':
           sum += nextNumber;
@@ -42,7 +36,6 @@ void main() {
           return;
       }
     }
-    // Natijani chiqaramiz
     print('Natija: $sum');
   } else {
     print('Ifoda kiritilmadi');
